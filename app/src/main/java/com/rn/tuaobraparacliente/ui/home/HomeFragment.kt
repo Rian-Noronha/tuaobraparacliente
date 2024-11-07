403c70552d070a212bc100e87fef98023a7fe44e
Rian-Noronha <riannoronha011@gmail.com>
2024-11-07 11:37:34 -0300

    [feat] add recycler demanda

--- app/src/main/java/com/rn/tuaobraparacliente/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rn/tuaobraparacliente/ui/home/HomeFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.rn.tuaobraparacliente.databinding.FragmentHomeBinding
@@ -12,9 +11,6 @@ import com.rn.tuaobraparacliente.databinding.FragmentHomeBinding
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -27,11 +23,6 @@ class HomeFragment : Fragment() {
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
